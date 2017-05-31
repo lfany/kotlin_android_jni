2c2e56d446d5b638e5939a5e895f106e46e9044b
hello <hello@ifnot.cc>
2017-05-31 17:08:39 +0800

    cleanup

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -9,3 +9,11 @@ Java_ifnot_cc_hello_MainActivity_stringFromJNI(
     std::string hello = "xxxxxxxxxx";
     return env->NewStringUTF(hello.c_str());
 }
+
+jstring
+Java_ifnot_cc_hello_MainActivity_string_hello(
+        JNIEnv* env,
+        jobject){
+        std::string hello = "it's good!";
+        return env->NewStringUTF(hello.c_str());
+}
---
+++ app/src/main/kotlin/ifnot/xx/hello/main.kt
@@ -0,0 +1,8 @@
+package ifnot.xx.hello
+
+class MainActivity : android.support.v7.app.AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
