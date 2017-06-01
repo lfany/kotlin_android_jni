1088ce6307d6619fc2e7d751142323a355a4fa2b
hello <hello@ifnot.cc>
2017-06-01 17:53:40 +0800

    fix

--- app/src/main/kotlin/ifnot/xx/hello/Main2Activity.kt
+++ app/src/main/kotlin/ifnot/xx/hello/Main2Activity.kt
@@ -2,6 +2,7 @@ package ifnot.xx.hello
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main22Activity : AppCompatActivity() {
@@ -15,6 +16,16 @@ class Main22Activity : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         title = stringHello()
         tv.text = stringHello()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_UP -> {
+                finish()
+                return true
+            }
+            else -> return super.onTouchEvent(event)
+        }
     }
 
     external fun stringHello(): String
