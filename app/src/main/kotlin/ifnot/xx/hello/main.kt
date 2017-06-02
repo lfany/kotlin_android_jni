7bddd1354e49b2d8103f07db00c76367fc785ea0
hello <hello@ifnot.cc>
2017-06-02 15:38:37 +0800

    update sentry

--- app/src/main/kotlin/ifnot/xx/hello/main.kt
+++ app/src/main/kotlin/ifnot/xx/hello/main.kt
@@ -2,6 +2,11 @@ package ifnot.xx.hello
 
 import android.content.Intent
 import android.view.MotionEvent
+import com.getsentry.raven.android.Raven
+import com.getsentry.raven.event.BreadcrumbBuilder
+import com.getsentry.raven.event.Breadcrumbs
+import com.getsentry.raven.event.Event
+import com.getsentry.raven.event.EventBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : android.support.v7.app.AppCompatActivity() {
@@ -14,12 +19,23 @@ class MainActivity : android.support.v7.app.AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         sample_text.text = stringFromJNI()
+
+        Raven.init(baseContext)
+
+        Raven.capture(EventBuilder()
+                .withMessage("Exception caught")
+                .withLevel(Event.Level.ERROR)
+        )
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action) {
             MotionEvent.ACTION_UP -> {
                 startActivity(Intent(this, Main22Activity::class.java))
+                Raven.capture("OnTouch Event")
+                Breadcrumbs.record(
+                        BreadcrumbBuilder().setMessage("User touched the window").build()
+                )
                 return true
             }
             else -> return super.onTouchEvent(event)
