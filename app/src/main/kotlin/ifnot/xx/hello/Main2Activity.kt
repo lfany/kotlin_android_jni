7bd17f961658157d2c175b5c760e505172065e85
hello <hello@ifnot.cc>
2017-06-22 17:56:37 +0800

    test

--- app/src/main/kotlin/ifnot/xx/hello/Main2Activity.kt
+++ app/src/main/kotlin/ifnot/xx/hello/Main2Activity.kt
@@ -3,10 +3,23 @@ package ifnot.xx.hello
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
 import hello.Hello
 import kotlinx.android.synthetic.main.activity_main2.*
 
-class Main22Activity : AppCompatActivity() {
+class Main22Activity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        Toast.makeText(v?.context, when (v?.id) {
+            btn_err.id -> Hello.reportError(btn_err.text.toString())
+            btn_crash.id -> Hello.reportPainc(btn_crash.text.toString())
+
+            else -> {
+                null
+            }
+        } ?: "点击了", Toast.LENGTH_SHORT)
+                .show()
+    }
 
     init {
         System.loadLibrary("native-lib")
@@ -19,6 +32,9 @@ class Main22Activity : AppCompatActivity() {
         tv.text = stringHello("Jni")
 
         tv2.text = Hello.greetings("go")
+
+        btn_err.setOnClickListener(this)
+        btn_crash.setOnClickListener(this)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
